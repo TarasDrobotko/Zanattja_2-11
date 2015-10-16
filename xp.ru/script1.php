7455ef6a915877f7e386e04dc50cf4cdc272899e
TarasDrobotko <drobotkot@gmail.com>
2015-10-16 18:03:57 +0300

    Operaczii

---
+++ xp.ru/script1.php
@@ -0,0 +1,16 @@
+<?PHP
+ $c = $_GET[‘c’];
+$d = $_GET[‘d’];
+switch($d)
+{ case ‘powr′:
+$rezult= pow($c,2);
+print $rezult;
+break;
+case ‘kw′:
+$result7 = sqrt($c);
+print $result7;
+break;
+
+}
+
+?>
